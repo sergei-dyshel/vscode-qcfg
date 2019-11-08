ddbd66d21708d453b2ef82eb325096ee26895b6b
Sergei Dyshel <qyron.private@gmail.com>
2019-11-08 18:09:33 +0200

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import', 'only-warn'],
+  extends: [
+    'airbnb',
+    'plugin:import/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint'
+  ],
+  env: {
+    es6: true
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts']
+      }
+    }
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified
+    // from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    strict: 'off',
+    curly: 'off',
+    'comma-dangle': 'off',
+    'import/prefer-default-export': 'off',
+    'nonblock-statement-body-position': 'off',
+    'import/no-unresolved': 'off',
+    'no-restricted-syntax': 'off',
+    'prefer-template': 'off',
+    'arrow-parens': 'off',
+    'max-classes-per-file': 'off',
+    'operator-linebreak': 'off',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'error'
+  }
+};
