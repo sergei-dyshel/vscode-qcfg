e456c40d58cc13422153bc725fc4a50ebbb7d3d8
Sergei Dyshel <qyron.private@gmail.com>
2020-01-19 09:42:14 +0200

    Remove peekOutline

--- src/peekOutline.ts
+++ src/peekOutline.ts
@@ -1,154 +1,20 @@
 import {
   ExtensionContext,
-  languages,
-  CallHierarchyProvider,
   TextDocument,
-  Position,
-  CancellationToken,
-  CallHierarchyItem,
-  CallHierarchyIncomingCall,
-  CallHierarchyOutgoingCall,
   commands,
   SymbolInformation,
   DocumentSymbol,
   Range,
   Location,
-  SymbolKind,
 } from 'vscode';
 import { Modules } from './module';
 import { getActiveTextEditor } from './utils';
 import { peekLocations } from './fileUtils';
 import { registerAsyncCommandWrapped } from './exception';
 import { offsetPosition } from './textUtils';
-import { log } from './logging';
 
 export type OutlineSymbol = SymbolInformation | DocumentSymbol;
 export type Outline = OutlineSymbol[];
-
-/**
- * Quickly browse outline hierarchy by using 'Peek Call Hierarchy' window
- */
-export async function peekOutlineHierarchy(outline: Outline) {
-  log.assert(!outlineToPeek);
-  outlineToPeek = outline;
-  try {
-    await commands.executeCommand('editor.showCallHierarchy');
-  } finally {
-    outlineToPeek = undefined;
-  }
-}
-
-// Private
-
-class OutlineRoot extends CallHierarchyItem {
-  constructor(public outline: Outline, public document: TextDocument) {
-    super(
-      SymbolKind.File,
-      '',
-      '',
-      document.uri,
-      new Range(0, 0, 0, 0),
-      new Range(0, 0, 0, 0),
-    );
-  }
-}
-
-function isDocumentSymbol(symbol: OutlineSymbol): symbol is DocumentSymbol {
-  return (
-    'detail' in symbol &&
-    'range' in symbol &&
-    'selectionRange' in symbol &&
-    'children' in symbol
-  );
-}
-
-class OutlineEntry extends CallHierarchyItem {
-  public symbol: SymbolInformation | DocumentSymbol;
-  public document: TextDocument;
-
-  constructor(
-    symbol: SymbolInformation | DocumentSymbol,
-    document: TextDocument,
-  ) {
-    if (isDocumentSymbol(symbol))
-      super(
-        symbol.kind,
-        symbol.name,
-        symbol.detail,
-        document.uri,
-        symbol.range,
-        symbol.selectionRange,
-      );
-    else
-      super(
-        symbol.kind,
-        symbol.name,
-        symbol.containerName,
-        symbol.location.uri,
-        symbol.location.range,
-        symbol.location.range,
-      );
-    this.symbol = symbol;
-    this.document = document;
-  }
-}
-
-let outlineToPeek: Outline | undefined;
-
-async function peekDocumentOutline() {
-  const document = getActiveTextEditor().document;
-  const outline = (await commands.executeCommand(
-    'vscode.executeDocumentSymbolProvider',
-    document.uri,
-  )) as Outline;
-  await peekOutlineHierarchy(outline);
-}
-
-const outlineHierarchyProvider: CallHierarchyProvider = {
-  async prepareCallHierarchy(
-    document: TextDocument,
-    _: Position,
-    __: CancellationToken,
-  ): Promise<CallHierarchyItem | undefined> {
-    if (!outlineToPeek) return undefined;
-    const outline = outlineToPeek;
-    outlineToPeek = undefined;
-    return new OutlineRoot(outline, document);
-  },
-
-  async provideCallHierarchyIncomingCalls(
-    _: CallHierarchyItem,
-    __: CancellationToken,
-  ): Promise<CallHierarchyIncomingCall[]> {
-    return [];
-  },
-
-  async provideCallHierarchyOutgoingCalls(
-    item: CallHierarchyItem,
-    __: CancellationToken,
-  ): Promise<CallHierarchyOutgoingCall[]> {
-    if (item instanceof OutlineRoot) {
-      return item.outline.map(symbol => {
-        const entry = new OutlineEntry(symbol, item.document);
-        return new CallHierarchyOutgoingCall(entry, [entry.selectionRange]);
-      });
-    }
-    if (item instanceof OutlineEntry) {
-      const symbol = item.symbol;
-      if (isDocumentSymbol(symbol))
-        return symbol.children.map(
-          child =>
-            new CallHierarchyOutgoingCall(
-              new OutlineEntry(child, item.document),
-              [child.selectionRange],
-            ),
-        );
-      if (symbol instanceof SymbolInformation) return [];
-      throw new Error('Unexpected symbol type');
-    }
-    throw new Error('Unexpected item type');
-  },
-};
 
 function findTextInRange(
   document: TextDocument,
@@ -186,12 +52,7 @@ async function peekFlatOutline() {
 
 function activate(context: ExtensionContext) {
   context.subscriptions.push(
-    languages.registerCallHierarchyProvider('*', outlineHierarchyProvider),
     registerAsyncCommandWrapped('qcfg.peekFlatOutline', peekFlatOutline),
-    registerAsyncCommandWrapped(
-      'qcfg.peekDocumentOutline',
-      peekDocumentOutline,
-    ),
   );
 }
 
