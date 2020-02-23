7250d3718501cc16c45a514595958f50f88c3429
Sergei Dyshel <qyron.private@gmail.com>
2020-02-23 08:18:38 +0200

    Fix quickpick outline
    Fixes handling of hierarchical DocumentSymbols.

--- src/extension/modules/peekOutline.ts
+++ src/extension/modules/peekOutline.ts
@@ -6,6 +6,7 @@ import {
   DocumentSymbol,
   Range,
   Location,
+  SymbolKind,
 } from 'vscode';
 import { Modules } from './module';
 import { getActiveTextEditor } from './utils';
@@ -28,6 +29,67 @@ function findTextInRange(
   return new Range(start, end);
 }
 
+function flattenSubtree(
+  symbol: DocumentSymbol,
+  document: TextDocument,
+  parent?: string,
+): Location[] {
+  if (!symbolIsBlock(symbol)) return [];
+  const newParent = parent ? parent + '.' + symbol.name : symbol.name;
+  const root = [narrowRange(symbol.name, symbol.selectionRange, document)];
+  if (shouldSkipChildren(symbol)) return root;
+  return root.concat(
+    ...symbol.children.map(child => flattenSubtree(child, document, newParent)),
+  );
+}
+
+function flattenOutline(
+  symbols: DocumentSymbol[],
+  document: TextDocument,
+): Location[] {
+  return ([] as Location[]).concat(
+    ...symbols.map(symbol => flattenSubtree(symbol, document)),
+  );
+}
+
+function symbolIsBlock(symbol: SymbolInformation | DocumentSymbol) {
+  switch (symbol.kind) {
+    case SymbolKind.Function:
+    case SymbolKind.Method:
+    case SymbolKind.Struct:
+    case SymbolKind.Class:
+    case SymbolKind.Enum:
+    case SymbolKind.Constructor:
+    case SymbolKind.Interface:
+      return true;
+    default:
+      return false;
+  }
+}
+
+function shouldSkipChildren(symbol: DocumentSymbol): boolean {
+  switch (symbol.kind) {
+    case SymbolKind.Interface:
+      return true;
+    default:
+      return false;
+  }
+}
+
+function narrowRange(name: string, range: Range, document: TextDocument) {
+  return new Location(
+    document.uri,
+    range.isSingleLine ? range : findTextInRange(document, name, range),
+  );
+}
+
+function isDocumentSymbol(
+  symbol: SymbolInformation | DocumentSymbol,
+): symbol is DocumentSymbol {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return (symbol as any).children !== undefined;
+}
+
 async function peekFlatOutline() {
   const editor = getActiveTextEditor();
   const document = editor.document;
@@ -35,18 +97,13 @@ async function peekFlatOutline() {
     'vscode.executeDocumentSymbolProvider',
     document.uri,
   )) as Outline;
-  const locations = outline.map(symbol => {
-    const range =
-      symbol instanceof SymbolInformation
-        ? symbol.location.range
-        : symbol.selectionRange;
-    return new Location(
-      document.uri,
-      range.isSingleLine
-        ? range
-        : findTextInRange(document, symbol.name, range),
-    );
-  });
+  if (outline.isEmpty) return;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const locations = isDocumentSymbol(outline[0])
+    ? flattenOutline(outline as DocumentSymbol[], document)
+    : (outline as SymbolInformation[])
+        .filter(symbolIsBlock)
+        .map(symbol => symbol.location);
   await peekLocations(locations);
 }
 
