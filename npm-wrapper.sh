e2d5fd5be1d7cfe98d3fa4fc9dfc6b5187528d5e
Sergei Dyshel <qyron.private@gmail.com>
2020-03-17 19:29:50 +0200

    Upgrade to vscode 1.43

--- npm-wrapper.sh
+++ npm-wrapper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export npm_config_target=6.1.2
+export npm_config_target=7.1.11
 export npm_config_disturl=https://atom.io/download/atom-shell
 export JOBS=$(nproc)
 npm --build-from-source "$@"
