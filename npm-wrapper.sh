99e4cbdbc5ab7cdeda845041c7a507788d5ac8d7
Sergei Dyshel <qyron.private@gmail.com>
2020-11-09 23:34:29 +0200

    Update to vscode 1.51.0

--- npm-wrapper.sh
+++ npm-wrapper.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-export npm_config_target=7.1.11
+export npm_config_target=9.3.3
 export npm_config_disturl=https://atom.io/download/atom-shell
 export JOBS=$(nproc)
+
+# prevent re2 installing binaries
+export RE2_DOWNLOAD_MIRROR=no
+
 npm --build-from-source "$@"
