34ee3ac6ea7badd74ac0402a7ee8ee409c39e69c
Sergei Dyshel <qyron.private@gmail.com>
2021-01-01 21:12:35 +0200

    Add Selection.reverse()

---
+++ src/extension/utils/selectionPrototype.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+import { Selection } from 'vscode';
+
+declare module 'vscode' {
+  export interface Selection {
+    reverse: () => Selection;
+  }
+}
+
+Selection.prototype.reverse = function (this: Selection): Selection {
+  return new Selection(this.active, this.anchor);
+};
