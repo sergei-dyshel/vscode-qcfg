c7c853553e6d5b8c81bc4c031b9bd88e0ca4a4b4
Sergei Dyshel <qyron.private@gmail.com>
2021-04-04 00:17:50 +0300

    Add revealSelection()
    Best effort to reveal editor selection

--- src/extension/modules/textUtils.ts
+++ src/extension/modules/textUtils.ts
@@ -35,7 +35,21 @@ export function selectRange(
   reversed?: boolean,
 ) {
   editor.selection = range.asSelection(reversed);
-  editor.revealRange(range);
+  revealSelection(editor);
+}
+
+/** Best effort to reveal selection */
+export function revealSelection(editor: TextEditor) {
+  const selections = editor.selections;
+  if (selections.length > 1) {
+    const start = selections.map((sel) => sel.start).min()!;
+    const end = selections.map((sel) => sel.end).min()!;
+    editor.revealRange(new Range(start, end));
+    return;
+  }
+  const selection = editor.selection;
+  editor.revealRange(selection);
+  editor.revealRange(selection.active.asRange);
 }
 
 export function trimWhitespace(document: TextDocument, range: Range) {
