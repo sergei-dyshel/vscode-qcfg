e7f6521112fbe58044303b98ebbcd2a9e20a4a13
Sergei Dyshel <qyron.private@gmail.com>
2022-04-05 09:14:22 +0300

    Misc small fixes

--- electron-node.sh
+++ electron-node.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
- export ELECTRON_RUN_AS_NODE=1
- exec /Applications/Code\ -\ OSS.app/Contents/MacOS/Electron "$@"
+export ELECTRON_RUN_AS_NODE=1
+exec /Applications/VSCodium.app/Contents/MacOS/Electron "$@"
+# exec /Applications/Code\ -\ OSS.app/Contents/MacOS/Electron "$@"
