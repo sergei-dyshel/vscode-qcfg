cd38571ffd51097e0ef8490301962bd70dc119d7
Sergei Dyshel <qyron.private@gmail.com>
2022-07-08 01:31:04 +0300

    syntaxNode.ts => syntaxNodePrototype.ts

--- src/library/syntax/index.ts
+++ src/library/syntax/index.ts
@@ -2,6 +2,6 @@ import { SyntaxLanguage } from './language';
 import './parsing'; // auguments classes
 // eslint-disable-next-line @typescript-eslint/no-duplicate-imports
 import { SyntaxTree } from './parsing';
-import { SyntaxNode } from './syntaxNode';
+import { SyntaxNode } from './syntaxNodePrototype';
 
 export { SyntaxNode, SyntaxLanguage, SyntaxTree };
