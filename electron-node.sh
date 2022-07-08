04280e1fe0909aac8217fa46b5337af661af0ffb
Sergei Dyshel <qyron.private@gmail.com>
2022-07-09 01:04:27 +0300

    Fix path to vscode electron

--- electron-node.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export ELECTRON_RUN_AS_NODE=1
-exec /Applications/VSCodium.app/Contents/MacOS/Electron "$@"
-# exec /Applications/Code\ -\ OSS.app/Contents/MacOS/Electron "$@"
