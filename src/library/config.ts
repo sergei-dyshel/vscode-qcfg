39a818ef87cdf88945a9fe30fbe33249ab8a311d
Sergei Dyshel <qyron.private@gmail.com>
2022-07-14 00:31:53 +0300

    alternate: switch to new config

--- src/library/config.ts
+++ src/library/config.ts
@@ -21,5 +21,55 @@ export namespace Config {
      * @default false
      */
     'qcfg.autoResize.enabled': boolean;
+
+    /**
+     * Mapping for alternate (header/source) switch.
+     *
+     * For each extension specify list of alternative extension.
+     * @default {}
+     */
+    'qcfg.alternate.mapping': Record<string, string[]>;
+
+    //   /**
+    //    * AutoSync enabled on start
+    //    * @default false
+    //    */
+    //   'qcfg.autoSync.enabled': boolean;
+
+    //   /** AutoSync command */
+    //   'qcfg.autoSync.command': string;
+
+    //   'qcfg.temp': Record<string, string>;
+
+    //   /** Log level */
+    //   'qcfg.log.level': 'debug' | 'info' | 'warning';
+
+    //   /** Open preview automatically when opening markdown documents */
+    //   'qcfg.autoMarkdownPreview': boolean;
+    //   /** Workspace folder name for creating new notes */
+    //   'qcfg.newNote.folder': string;
+    //   /** Path of notes directory relative to workspace folder root */
+    //   'qcfg.newNote.path': string;
+
+    //   'qcfg.git.web': Git.Entry;
   }
+
+  // export namespace Git {
+  //   export interface Link {
+  //     /** Description of Web link */
+  //     title: string;
+  //     /** Web url */
+  //     url: string;
+  //   }
+
+  //   /**
+  //    * List of rules to open current line in Git Web UI
+  //    */
+  //   export interface Entry {
+  //     /** List of remote patterns */
+  //     remotes: string[];
+  //     /** Description of Web link */
+  //     links: Link[];
+  //   }
+  // }
 }
