c1e639aec293390c666fc8a468c72dd349dfe41c
Sergei Dyshel <qyron.private@gmail.com>
2022-08-03 23:32:35 +0300

    New way to update package.json and install
    Instead of composing it from individual files, now patch it in-place
    with config/commands/keybindings from files.
    
    No more syncing common.json!
    
    Each build now creates tagged commit with bumped version and resulting
    vsix.

--- src/library/json.ts
+++ src/library/json.ts
@@ -1,3 +1,19 @@
+import * as jsonc from 'jsonc-parser';
+
+/**
+ * Wrapper over `jsonc-parser` to edit JSONC files
+ */
+export class JsoncEditor {
+  options: jsonc.ModificationOptions = {};
+
+  constructor(public text: string) {}
+
+  modify(jsonPath: jsonc.JSONPath, value: unknown) {
+    const edits = jsonc.modify(this.text, jsonPath, value, this.options);
+    this.text = jsonc.applyEdits(this.text, edits);
+  }
+}
+
 /**
  * JSON types
  */
