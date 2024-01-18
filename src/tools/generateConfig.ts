ad88d3c8579543da613613385d44da9958215b3f
Sergei Dyshel <qyron.private@gmail.com>
2024-01-18 16:12:23 +0530

    Use resource scope for tasks config key
    To allow per-forlder tasks.

--- src/tools/generateConfig.ts
+++ src/tools/generateConfig.ts
@@ -1,6 +1,7 @@
 import * as tsj from 'ts-json-schema-generator';
 import type { ExtensionJSON } from '../library/extensionManifest';
 import * as nodejs from '../library/nodejs';
+import type { Config } from '../library/config';
 
 const [path, type, out] = nodejs.process.argv.slice(2);
 
@@ -16,6 +17,10 @@ const config: tsj.Config = {
 const generator = tsj.createGenerator(config);
 
 const schema = generator.createSchema(config.type);
+
+const properties = schema.properties! as unknown as Config.All;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(properties['qcfg.tasks'] as any).scope = 'resource';
 
 const pkg: ExtensionJSON.Manifest = {
   contributes: {
